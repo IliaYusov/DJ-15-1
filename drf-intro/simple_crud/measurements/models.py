0bbbb5e35f5def549d3affa07b0c904ebb16ca11
Ilia Yusov <ouster2k@gmail.com>
2021-03-01 01:58:23 +0300

    sixth homework

--- drf-intro/simple_crud/measurements/models.py
+++ drf-intro/simple_crud/measurements/models.py
@@ -1,28 +1,27 @@
 from django.db import models
 
 
-class Project(models.Model):
-    """Объект на котором проводят измерения."""
+class TimestampFields(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
 
+    class Meta:
+        abstract = True  # без этого флага будет создано отношение в базе данных
+
+
+class Project(TimestampFields):  # наследование без abstract будет с помощью автоматического создания OneToOne поля
+    """Объект на котором проводят измерения."""
     name = models.TextField()
     latitude = models.FloatField()
     longitude = models.FloatField()
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
 
 
-class Measurement(models.Model):
+class Measurement(TimestampFields):
     """Измерение температуры на объекте."""
-
     value = models.FloatField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
+    image = models.ImageField(null=True, default=None)
--- drf-intro/simple_crud/measurements/views.py
+++ drf-intro/simple_crud/measurements/views.py
@@ -1,11 +1,15 @@
+from measurements.models import Measurement, Project
+from measurements.serializers import ProjectSerializer, MeasurementSerializer
 from rest_framework.viewsets import ModelViewSet
 
 
 class ProjectViewSet(ModelViewSet):
     """ViewSet для проекта."""
-    # TODO: добавьте конфигурацию для объекта
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
 
 
 class MeasurementViewSet(ModelViewSet):
     """ViewSet для измерения."""
-    # TODO: добавьте конфигурацию для измерения
+    serializer_class = MeasurementSerializer
+    queryset = Measurement.objects.all()
